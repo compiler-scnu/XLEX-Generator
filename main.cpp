0f5307d31ca3ac6983c9c09b2abeeba4a774d765
someone-chan <1830882822@qq.com>
2020-11-01 23:19:04 +0800

    主界面最大化

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+    w.setWindowTitle("XLEX生成器");
+    w.showMaximized();
     return a.exec();
 }
